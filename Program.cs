b2d0cec6ed1cebc214ea9d7e4b22fdbc02a5adb1
NesibeHatzi <37589455+NesibeHatzi@users.noreply.github.com>
2018-05-27 19:37:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Aufgabe5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public class Person
+        {
+            string name;
+            int age;
+        }
+
+        public class Teilnehmer : Person
+        {
+            int registrationNumber;
+            string[] cours; 
+        
+
+            }
+        
+        public class Dozent : Person
+        {
+            int room;
+            DateTime office_hours;
+            string[] cours;
+            public void listCourse(){
+                 
+            Console.WriteLine(Course.title);
+            }
+            public List <Member> listMember ();
+                 
+            }
+        }
+
+        public class Course
+        {
+            string title;
+            DateTime course_hours;
+            int room;
+            string [] dozent;
+            string [] members;
+            
+           
+        
+     public void Infotext()
+        {
+            Console.WriteLine("Der Kurs " + title + " findet am " + course_hours + " in Raum " + room + " statt.");
+}
+    
+        }
+    
+}
